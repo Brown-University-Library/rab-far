a8aeae30039ae7f758fd98a91e808159d1704cdb
Steven McCauley <Steven_McCauley@brown.edu>
2018-03-02 12:20:50 -0500

    Loop through tables, download contents

--- dump-tables.sh
+++ dump-tables.sh
@@ -1,3 +1,10 @@
 source dump-tables.vars.sh
 
-mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "SHOW TABLES" > data/far_tables.txt
+mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "SHOW TABLES" > ${DATADIR}/${TABLES}
+
+tail --lines=+2 ${DATADIR}/${TABLES} | while IFS='' read -r table
+do
+  echo "$table" && \
+  mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "SELECT * FROM $table" > ${DATADIR}/$table.csv && \
+  sleep 2;
+done
