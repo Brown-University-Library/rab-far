98e3ab932b2f755c51cbf1fc12ed4118126e6dba
Steven McCauley <Steven_McCauley@brown.edu>
2018-03-07 14:55:11 -0500

    Fix INFORMATION_SCHEMA query

--- dump-tables.sh
+++ dump-tables.sh
@@ -13,7 +13,7 @@ mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "SHOW TABLES" > 
 tail --lines=+2 data/far-tables.txt | while IFS='' read -r table
 do
   echo "Get fields: $table";
-  mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "select GROUP_CONCAT(COLUMN_NAME SEPARATOR '\`,\`') from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='${table}'" > data/field-data.txt;
+  mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "select GROUP_CONCAT(COLUMN_NAME SEPARATOR '\`,\`') from INFORMATION_SCHEMA.COLUMNS where TABLE_SCHEMA='${DBNAME}' AND TABLE_NAME='${table}'" > data/field-data.txt;
   fields=$(tail -1 data/field-data.txt);
   echo "${table}|\`${fields}\`" >> data/far-fields.txt && \
   sleep 0.5;
@@ -21,7 +21,7 @@ done
 
 while IFS='|' read -r table fields
 do
-  echo "Selecting ${fields} from ${table}" && \
+  echo "${table} : ${fields}" && \
   mysql -h ${DBLOC} -u ${USER} --password="${PASSW}" ${DBNAME} -e "select CONCAT_WS('~@~',${fields}) FROM ${table}" > data/far-tables/$table.csv && \
   sleep 1;
 done < data/far-fields.txt
