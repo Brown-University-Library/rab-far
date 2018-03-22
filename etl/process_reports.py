cfe60d7f778b821afcf5e9ce71c5379a9a0e7344
Steven McCauley <steven_mccauley@brown.edu>
2018-03-22 12:12:37 -0400

    Cleanup process, refactor mysql query

--- etl/process_reports.py
+++ etl/process_reports.py
@@ -10,51 +10,43 @@ import mysql.connector
 
 from config.settings import config
 
-def join_far_tables(dbConnection):
-    query = """
+def mysql_query(func):
+
+    def query(*args, **kwargs):
+        try:
+            cnx = mysql.connector.connect(
+                user=config['MYSQL_USR'], password=config['MYSQL_PASSW'],
+                database=config['MYSQL_FAR'])
+            cursor = cnx.cursor()
+            cursor.execute( func(*args, **kwargs) )
+            data = [ row for row in cursor ]
+            cursor.close()
+            cnx.close()
+        except:
+            raise
+        return data
+
+    return query
+
+@mysql_query
+def get_consenting_reports():
+    return """
         SELECT u.id AS bruid, u.email, r.id AS report_id
         FROM users AS u
         JOIN activity_reports AS r ON (u.id = r.user_id)
         JOIN consent_report AS c ON (c.activity_report_id = r.id)
         WHERE c.consent_type_id="library"
     """
-    cursor = dbConnection.cursor()
-    cursor.execute(query)
-    data = [ row for row in cursor ]
-    cursor.close()
-    return data
+@mysql_query
+def get_far_table(table, columns=['*']):
+    return "SELECT {0} FROM {1}".format(','.join(columns), table)
 
-def get_far_data(columns, table, cursor):
-    # report_rows = get_far_data(
-    #     ['id', 'user_id'], 'activity_reports', crsr)
-    # consent_rows = get_far_data(
-    #     ['activity_report_id','consent_type_id'],
-    #     'consent_report', crsr)
-    # user_rows = get_far_data(['*'],'users', crsr)
-    query = "SELECT {0} FROM {1}".format(','.join(columns), table)
-    cursor.execute(query)
-    data = [ row for row in cursor ]
-    return data
 
-def filter_consenting_reports(rows):
-    consent_idx = 1
-    col_vals = { row[consent_idx] for row in rows }
-    assert col_vals == { 'centers', 'library', 'ovpr', 'swearer' }
-    return [ row for row in rows
-                if row[consent_idx] == 'library' ]
-
-def user_id_lookup(row, idPairs):
-    bruid_idx = 0
+def get_rab_identites():
+    mongo = pymongo.MongoClient(config['MONGO_ADDR'])
+    id_db = mongo.get_database(config['MONGO_IDS'])
     try:
-        res = mongoColl.find_one({'bruid' : row[bruid_idx]})
-        shortid = res['shortid']
-        return (True, row)
-    except:
-        return (False, row)
-
-def get_rab_identites(mongoColl):
-    try:
-        cursor = mongoColl.find({'bruid' : {'$ne' : ''} },
+        cursor = id_db['rabids'].find({'bruid' : {'$ne' : ''} },
             {'_id': False, 'bruid': True, 'rabid': True})
         id_pairs = [ (doc['bruid'], doc['rabid']) for doc in cursor ]
         cursor.close()
@@ -62,42 +54,18 @@ def get_rab_identites(mongoColl):
         raise
     return id_pairs
 
-def map_ids(farRows, mongoIds):
-    id_map = { bruid : rabid for bruid, rabid in mongoIds }
+def map_farids_to_rabids(farRows, rabIds):
+    id_map = { bruid : rabid for bruid, rabid in rabIds }
     mapped = [ (id_map[bruid], bruid, email, report_id)
         for bruid, email, report_id in farRows if bruid in id_map ]
     return mapped
 
-def filter_report_data(consentRows, reportRows):
-    report_idx = 0
-    rprt_id_idx = 0
-    consenting_report_ids = { row[report_idx] for row in consentRows }
-    return [ row for row in reportRows
-                if row[rprt_id_idx] in consenting_report_ids]
-
 def process():
-    cnx = mysql.connector.connect(
-        user=config['MYSQL_USR'], password=config['MYSQL_PASSW'],
-        database=config['MYSQL_FAR'])
-    mongo = pymongo.MongoClient(config['MONGO_ADDR'])
-    id_db = mongo.get_database(config['MONGO_IDS'])
-    id_coll = id_db['rabids']
-    user_rows = join_far_tables(cnx)
-    rabids = get_rab_identites(id_coll)
-    rab_users = map_ids(user_rows, rabids)
-    queryable_tables = ['articles', 'books', 'chapters',
-        'papers', 'patents']
-    # print(len(bad))
-    # print(bad[:10])
-    # print(len(shortids))
-    # print(shortids[:10])
-    # cnst_data = filter_consenting_faculty(consent_rows)
-    # allowed_reports = filter_consenting_reports(
-    #     cnst_data, report_rows)
-    # consenting = get_user_data(cnst_data, report_rows, user_rows)
-    cnx.close()
-    mongo.close()
-    return rab_users
+    user_reports = get_consenting_reports()
+    rabids = get_rab_identites()
+    reports_with_rabids = map_farids_to_rabids(user_reports, rabids)
+
+    return reports_with_rabids
 
 if __name__ == '__main__':
     pass
